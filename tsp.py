1be73eccfd110a3314cc28dae5a8e78a4dcca9bb
Sergio <ssouto@guidepoint.com>
2025-10-02 21:27:24 -0300

    changed initial setup

--- tsp.py
+++ tsp.py
@@ -36,23 +36,23 @@ BLUE = (0, 0, 255)
 #                     for _ in range(N_CITIES)]
 
 
-# # Using Deault Problems: 10, 12 or 15
-WIDTH, HEIGHT = 800, 400
-cities_locations = default_problems[15]
+# # # Using Deault Problems: 10, 12 or 15
+# WIDTH, HEIGHT = 800, 400
+# cities_locations = default_problems[15]
 
 
 # Using att48 benchmark
-# WIDTH, HEIGHT = 1500, 800
-# att_cities_locations = np.array(att_48_cities_locations)
-# max_x = max(point[0] for point in att_cities_locations)
-# max_y = max(point[1] for point in att_cities_locations)
-# scale_x = (WIDTH - PLOT_X_OFFSET - NODE_RADIUS) / max_x
-# scale_y = HEIGHT / max_y
-# cities_locations = [(int(point[0] * scale_x + PLOT_X_OFFSET),
-#                      int(point[1] * scale_y)) for point in att_cities_locations]
-# target_solution = [cities_locations[i-1] for i in att_48_cities_order]
-# fitness_target_solution = calculate_fitness(target_solution)
-# print(f"Best Solution: {fitness_target_solution}")
+WIDTH, HEIGHT = 1500, 800
+att_cities_locations = np.array(att_48_cities_locations)
+max_x = max(point[0] for point in att_cities_locations)
+max_y = max(point[1] for point in att_cities_locations)
+scale_x = (WIDTH - PLOT_X_OFFSET - NODE_RADIUS) / max_x
+scale_y = HEIGHT / max_y
+cities_locations = [(int(point[0] * scale_x + PLOT_X_OFFSET),
+                     int(point[1] * scale_y)) for point in att_cities_locations]
+target_solution = [cities_locations[i-1] for i in att_48_cities_order]
+fitness_target_solution = calculate_fitness(target_solution)
+print(f"Best Solution: {fitness_target_solution}")
 # ----- Using att48 benchmark
 
 
